7acecc61ac84a3844357e90d616c0bb95424e271
sangtaik <bluestruggle@naver.com>
2022-03-16 17:59:43 +0900

    seaborn part update

---
+++ pandas/hangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import matplotlib as plt
+from matplotlib import rc
+rc('font', family = 'Malgun Gothic') #
+plt.rcParams['axes.unicode_minus'] = False
+
